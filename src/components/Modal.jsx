98990251eb7e5285e4f69fbde8fcbdab5ac4fd42
Chie <chie@nativshark.com>
2023-02-06 09:02:56 +0900

    images can be clicked on and full screened

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+const Image = ({ src, alt }) => {
+  return <img src={src} alt={alt} />;
+};
 
-export default function Modal({ modal, setModal, children }) {
+export default function Modal({ modal, setModal, src, alt }) {
   if (!modal) return null;
 
   return (
     <>
       <div
-        class="flex justify-center items-center fixed bg-jade opacity-90 right-0 left-0 top-0 bottom-0"
+        className="flex justify-center items-center fixed  right-0 left-0 top-0 bottom-0"
         onClick={() => setModal(false)}
       >
-        <div class="bg-neutral-50 w-1/2 h-1/2 m-auto">
-          <button onClick={() => setModal(false)}>Close</button>
-          {children}
+        <div className="bg-neutral-700 opacity-80 absolute right-0 left-0 top-0 bottom-0 -z-10" />
+        <div className="flex flex-col max-w-[800px] items-center m-auto">
+          {/* <button onClick={() => setModal(false)}>Close</button> */}
+          <div className="h-16" />
+          <Image src={src} alt={alt} />
+          <p className="mt-4 text-jade-50">{alt}</p>
         </div>
       </div>
     </>
--- src/components/ModalButton.jsx
+++ src/components/ModalButton.jsx
@@ -1,21 +1,22 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Modal from "./Modal";
 
 // children is anything that is being passed in as a prop
-export default function ModalButton({ children }) {
+export default function ModalButton({ src, alt, className }) {
   const [modal, setModal] = useState(false);
   // when imported on other pages ex: <ModalButton>children here</ModalButton>
   return (
     <div>
       <button
+        className=""
         onClick={() => {
           setModal(!modal);
           console.log(modal);
         }}
       >
-        Open Modal
+        <img className={className} src={src} alt={alt} />
       </button>
-      <Modal modal={modal} setModal={setModal} children={children} />
+      <Modal modal={modal} setModal={setModal} src={src} alt={alt}></Modal>
     </div>
   );
 }
